6f43cabd0631d696d478ce0104dff1427957fc5d
mrdavbu <mrdavbu@gmail.com>
2026-08-10 17:40:47 -0500

    Delete api directory

--- api/verify.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', 'POST');
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-  const code = req.body?.code || '';
-  const ok = !!process.env.EDITOR_CODE && code === process.env.EDITOR_CODE;
-  return res.json({ ok });
-}
